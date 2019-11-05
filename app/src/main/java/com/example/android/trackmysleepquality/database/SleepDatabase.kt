f775866ca9efec5cc6bd25195436b7e7775b2d53
Brianna Magnuson <54729913+Brianna-M-93@users.noreply.github.com>
2019-11-04 18:45:12 -0800

    created a RoomDatabase, added an abstract class to extend the database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -16,22 +16,9 @@
 
 package com.example.android.trackmysleepquality.database
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.Database
+import androidx.room.RoomDatabase
 
-@Entity(tableName = "daily_sleep_quality_table")
-data class SleepNight (
-        //var value can change, val is a constant
-        @PrimaryKey(autoGenerate = true)
-        var nightId: Long = 0L,
-
-        @ColumnInfo(name = "start_time_milli")
-        val startTimeMilli: Long = System.currentTimeMillis(),
-
-        @ColumnInfo(name = "end_time_milli")
-        var endTimeMilli: Long = startTimeMilli,
-
-        @ColumnInfo(name = "quality_rating")
-        var sleepQuality: Int = -1
-)
+        //list the tables              //schema version       //saves the schema to a folder that shows version history for the database
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)    //true by default
+abstract class SleepDatabase : RoomDatabase(){}
